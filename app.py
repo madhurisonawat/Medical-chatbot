cc4610fc3890e7e492fe09e4c0e891c5bf6eb76c
madhurisonawat <madhurisonawat@gmail.com>
2026-07-15 21:28:23 +0530

    Added dummy code

--- app.py
+++ app.py
@@ -1,85 +1,12 @@
-from flask import Flask, render_template, jsonify, request
-from src.helper import download_embeddings
-from langchain_pinecone import PineconeVectorStore
-from langchain_groq import ChatGroq
-from langchain_classic.chains import create_retrieval_chain
-from langchain_classic.chains.combine_documents import (
-    create_stuff_documents_chain,
-)
-from langchain_core.prompts import ChatPromptTemplate
-from dotenv import load_dotenv
-from src.prompt import *
-import os
-
+from flask import Flask
 
 app = Flask(__name__)
 
-
-load_dotenv()
-
-rag_chain = None
-
-
-def get_rag_chain():
-    global rag_chain
-
-    if rag_chain is None:
-        embeddings = download_embeddings()
-
-        docsearch = PineconeVectorStore.from_existing_index(
-            index_name="medical-chatbot",
-            embedding=embeddings
-        )
-
-        retriever = docsearch.as_retriever(
-            search_type="similarity",
-            search_kwargs={"k": 3}
-        )
-
-        llm = ChatGroq(
-            model="qwen/qwen3-32b",
-            reasoning_effort="none"
-        )
-
-        prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", system_prompt),
-                ("human", "{input}")
-            ]
-        )
-
-        qa_chain = create_stuff_documents_chain(
-            llm,
-            prompt
-        )
-
-        rag_chain = create_retrieval_chain(
-            retriever,
-            qa_chain
-        )
-
-    return rag_chain
-
-
-
-
 @app.route("/")
-def index():
-    return render_template('chat.html')
-
-
-chain = get_rag_chain()
-@app.route("/get", methods=["GET", "POST"])
-def chat():
-    msg = request.form["msg"]
-    input = msg
-    print(input)
-    response = chain.invoke({"input": msg})
-    print("Response : ", response["answer"])
-    return str(response["answer"])
-
-
+def home():
+    return "Medical Chatbot is running!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    import os
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
