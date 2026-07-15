4da294d2ba9c400663d86e029df3b73a5f184674
madhurisonawat <madhurisonawat@gmail.com>
2026-07-16 01:11:40 +0530

    added fix

--- app.py
+++ app.py
@@ -11,8 +11,10 @@ from dotenv import load_dotenv
 from src.prompt import *
 import os
 
-print("PINECONE_API_KEY:", os.getenv("PINECONE_API_KEY"))
-print("Length:", len(os.getenv("PINECONE_API_KEY", "")))
+import os
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 app = Flask(__name__)
 
